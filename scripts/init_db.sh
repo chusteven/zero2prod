46d80cb0423d1a526de34b1b79a529190f56aa3f
Steven Chu <stevenc1@gmail.com>
2021-11-11 08:48:41 -0800

    getting ready for deployment!!

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -42,7 +42,8 @@ done
 
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
 
-export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+# https://github.com/docker/for-mac/issues/2965
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@kubernetes.docker.internal:${DB_PORT}/${DB_NAME}
 sqlx database create
 sqlx migrate run
 
--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,15 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
+    let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string())
         .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).expect("Failed to bind to port 8000");
     run(listener, connection_pool)?.await
 }
