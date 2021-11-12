1015eb9499f00185f28787f7ba95caa498628f8c
Steven Chu <stevenc1@gmail.com>
2021-11-12 08:25:34 -0800

    docstrings

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -12,7 +12,7 @@ use tracing_subscriber::{fmt::MakeWriter, layer::SubscriberExt, EnvFilter, Regis
 /// spell out the actual type of the returned subscriber, which is
 /// indeed quite complex.
 /// We need to explicitly call out that the returned subscriber is
-/// `Send`and `Sync`to make it possible to pass it to `init_subscriber`
+/// `Send` and `Sync` to make it possible to pass it to `init_subscriber`
 /// later on.
 pub fn get_subscriber(
     name: String,
